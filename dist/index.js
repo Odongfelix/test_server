b397dd02beea5e313a44ff8df6c4ef7dd644cdd3
odong felix <odongfelix61@gmail.com>
2026-05-24 22:50:01 +0300

    public host

--- dist/index.js
+++ dist/index.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const server = (0, express_1.default)();
-server.listen(3000, (e) => {
+server.listen(3000, '0.0.0.0', (e) => {
     if (e)
         throw e;
     console.log('Server is running on port 3000');
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import e from 'express'
 
 const server = e()
 
-server.listen(3000,(e)=>{
+server.listen(3000,'0.0.0.0',(e)=>{
     if(e) throw e
     console.log('Server is running on port 3000')
 })
